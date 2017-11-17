e5399f3ef08409f765014a29716ee440984ec20e
Alex Waychoff <AlexWaychoff@gmail.com>
2017-11-17 13:09:40 -0600

    Asking for player or computer works, added ToLower to get a valid response

--- RPSLS/AssembleGame.cs
+++ RPSLS/AssembleGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace RPSLS
 {
@@ -14,7 +15,7 @@ namespace RPSLS
         public string AskForOpponent()
         {
             userInput = VerifyInput("Are you playing against another human?  Type 'y' or type 'n' to play against a computer.");
-            return "remove later";
+            return userInput;
         }
         
         public string VerifyInput(string question)
@@ -27,10 +28,11 @@ namespace RPSLS
                     Console.WriteLine("\n**Your input wasn't valid, please type exactly as indicated.**\n");
                 }
                 Console.WriteLine(question);
-                userInput = Console.ReadKey().Key.ToString();
+                userInput = Console.ReadKey().Key.ToString().ToLower();
+                Console.WriteLine(userInput);
                 checkCorrectInput = true;
             }
-            while (!(userInput == "y") || !(userInput == "n"));
+            while (!(userInput == "y" || userInput == "n"));
             return userInput;
         }
     }
